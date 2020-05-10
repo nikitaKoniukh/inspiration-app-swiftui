3fb933dc168c18c6538db45a71584ee7f367ab6d
Nikita Koniukh <nikita@msapps>
2020-05-10 21:36:51 +0300

    Initial Commit

---
+++ Inspiration App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Inspiration App
+//
+//  Created by Nikita Koniukh on 10/05/2020.
+//  Copyright © 2020 Nikita Koniukh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
